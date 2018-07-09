bd9569635b825739c9c0e61b4d11caee1a0e67ed
waiting <1661926154@qq.com>
2018-07-09 21:20:23 +0800

    test: add 30_func.test.ts

---
+++ test/30_func.test.ts
@@ -0,0 +1,82 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+import { Observable } from 'rxjs'
+
+import { walk, DirFilterCbParams, DirFilterCbRet, EntryType, WalkEvent } from '../src/index'
+import {
+  basename,
+  join,
+} from '../src/shared/index'
+
+const filename = basename(__filename)
+const mods = rewire('../src/lib/index')
+
+
+describe(filename, () => {
+
+  it('Should handleError() works with EPERM', resolve => {
+    const fnName = 'handleError'
+    const fn = <(err: NodeJS.ErrnoException) => Observable<WalkEvent>> mods.__get__(fnName)
+
+    if (typeof fn !== 'function') {
+      return assert(false, `${fnName} is not a function`)
+    }
+    const err: NodeJS.ErrnoException = new Error()
+
+    err.code = EntryType.noAcessPermission
+    err.path = Math.random() + ''
+
+    fn(err).subscribe(
+      data => {
+        assert(data.type === EntryType.noAcessPermission)
+        assert(data.path === err.path)
+        resolve()
+      },
+    )
+  })
+
+  it('Should handleError() works with ENOENT', resolve => {
+    const fnName = 'handleError'
+    const fn = <(err: NodeJS.ErrnoException) => Observable<WalkEvent>> mods.__get__(fnName)
+
+    if (typeof fn !== 'function') {
+      return assert(false, `${fnName} is not a function`)
+    }
+    const err: NodeJS.ErrnoException = new Error()
+
+    err.code = EntryType.notExist
+    err.path = Math.random() + ''
+
+    fn(err).subscribe(
+      data => {
+        assert(data.type === EntryType.notExist)
+        assert(data.path === err.path)
+        resolve()
+      },
+    )
+  })
+
+  it('Should handleError() works with unknown', resolve => {
+    const fnName = 'handleError'
+    const fn = <(err: NodeJS.ErrnoException) => Observable<WalkEvent>> mods.__get__(fnName)
+
+    if (typeof fn !== 'function') {
+      return assert(false, `${fnName} is not a function`)
+    }
+    const err: NodeJS.ErrnoException = new Error()
+
+    err.code = EntryType.unknown
+    err.path = Math.random() + ''
+
+    fn(err).subscribe(
+      data => {
+        assert(data.type === EntryType.unknown)
+        assert(data.path === err.path)
+        resolve()
+      },
+    )
+  })
+
+})
