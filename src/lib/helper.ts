0c372282b4e477d8515d42daf706946f653d19c9
waiting <1661926154@qq.com>
2022-06-06 18:11:56 +0800

    refactor: with lib/helper.ts

---
+++ src/lib/helper.ts
@@ -0,0 +1,202 @@
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
+import { lstat, readdir, Stats } from 'node:fs'
+import { readlink } from 'node:fs/promises'
+
+import {
+  from as ofrom,
+  merge,
+  of,
+  Observable,
+  Observer, EMPTY,
+} from 'rxjs'
+import { catchError, filter, mergeMap } from 'rxjs/operators'
+
+import { initialWalkEvent } from './config.js'
+import {
+  DirFilterCb,
+  DirFilterCbParams,
+  EntryType,
+  Filename,
+  Filepath,
+  Options,
+  WalkEvent,
+  WalkFnParams,
+} from './model.js'
+
+
+export function entryProxy(
+  path: string,
+  options: Options,
+  curDepth: number,
+  parentPath: Filepath,
+): Observable<WalkEvent> {
+
+  const stats$ = Observable.create((obv: Observer<Stats>) => {
+    lstat(path, (err, stats) => {
+      if (err) {
+        return obv.error(err)
+      }
+      obv.next(stats)
+      obv.complete()
+    })
+  }) as Observable<Stats>
+
+  return stats$.pipe(
+    mergeMap((stats: Stats) => _entryProxy(path, stats, options, curDepth, parentPath)),
+    catchError((err: Error | NodeJS.ErrnoException) => handleError(err, curDepth)),
+  )
+}
+
+// eslint-disable-next-line max-params
+function _entryProxy(
+  path: string,
+  stats: Stats,
+  options: Options,
+  curDepth: number,
+  parentPath: Filepath,
+): Observable<WalkEvent> {
+
+  if (stats.isFile()) {
+    return of<WalkEvent>({
+      ...initialWalkEvent,
+      depth: curDepth,
+      type: EntryType.file,
+      path,
+      parentPath,
+      stats,
+    })
+  }
+
+  if (stats.isDirectory()) {
+    const ret: WalkEvent = {
+      ...initialWalkEvent,
+      depth: curDepth,
+      type: EntryType.dir,
+      path,
+      parentPath,
+      stats,
+    }
+
+    return merge(
+      of(ret),
+      curDepth < options.maxDepth ? walkDir({ path, options, curDepth: curDepth + 1 }) : EMPTY,
+    )
+  }
+
+  // will be trigger both EntryType.link, and EntryType.file | EntryType.dir later
+  if (stats.isSymbolicLink()) {
+    const ret: WalkEvent = {
+      ...initialWalkEvent,
+      depth: curDepth,
+      type: EntryType.link,
+      path,
+      parentPath,
+      stats,
+    }
+
+    return merge(
+      of(ret),
+      options.followLink && curDepth < options.maxDepth
+        ? walkLink({ path, options, curDepth })
+        : EMPTY
+      ,
+    )
+  }
+
+  return of(initialWalkEvent)
+}
+
+
+export function procDirfilterCb(cb: DirFilterCb, ps: DirFilterCbParams): Observable<Filepath> {
+  const filterRet = cb(ps)
+
+  if (! filterRet) {
+    return EMPTY
+  }
+  else if (filterRet instanceof Observable) {
+    return filterRet
+  }
+  else if (Array.isArray(filterRet) && filterRet.length) {
+    return ofrom(filterRet)
+  }
+  else if (filterRet instanceof Promise) {
+    return ofrom(filterRet).pipe(
+      mergeMap((files: void | string[]) => {
+        return files ? ofrom(files) : EMPTY
+      }),
+    )
+  }
+
+  return EMPTY
+}
+
+export function handleError(err: NodeJS.ErrnoException, curDepth: number): Observable<WalkEvent> {
+  let entryType = EntryType.unknown
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (err && err.code) {
+    switch (err.code) {
+      case EntryType.notExist:
+        entryType = EntryType.notExist
+        break
+
+      case EntryType.noAcessPermission:
+        entryType = EntryType.noAcessPermission
+        break
+    }
+  }
+
+  return of<WalkEvent>({
+    ...initialWalkEvent,
+    type: entryType,
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    path: err && err.path ? err.path : '',
+    parentPath: '',
+    depth: curDepth,
+    error: err,
+  })
+}
+
+
+export function walkDir({ path, options, curDepth }: WalkFnParams): Observable<WalkEvent> {
+  const useFilter = typeof options.dirFilterCb === 'function'
+  const files$ = Observable.create((obv: Observer<Filename[]>) => {
+    readdir(path, (err, files) => {
+      if (err) {
+        return obv.error(err)
+      }
+      obv.next(files)
+      obv.complete()
+    })
+  }) as Observable<Filename[]>
+
+  const ret$ = files$.pipe(
+    mergeMap((files) => {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (useFilter && options.dirFilterCb) {
+        return procDirfilterCb(options.dirFilterCb, {
+          parentPath: path,
+          files,
+          curDepth,
+        })
+      }
+      return ofrom(files)
+    }),
+    filter((file) => {
+      return !! (file && typeof file === 'string')
+    }),
+    mergeMap((file: string) => {
+      return entryProxy(path + '/' + file, options, curDepth, path)
+    }),
+  )
+
+  return ret$
+}
+
+
+export function walkLink({ path, options, curDepth }: WalkFnParams): Observable<WalkEvent> {
+  return ofrom(readlink(path)).pipe(
+    mergeMap(file => entryProxy(file, options, curDepth, path)),
+  )
+}
+
--- test/30.func.test.ts
+++ test/30.func.test.ts
@@ -1,31 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable import/newline-after-import */
 import assert from 'node:assert/strict'
 
 import { fileShortPath } from '@waiting/shared-core'
-import rewire from 'rewire'
 
 import { EntryType } from '../src/index.js'
+import { handleError } from '../src/lib/helper.js'
 
-
-const mods = rewire('../src/lib/index')
 
 describe(fileShortPath(import.meta.url), () => {
 
-  it('Should handleError() works with EPERM', (resolve) => {
-    const fnName = 'handleError'
-    const fn = mods.__get__(fnName)
-
-    if (typeof fn !== 'function') {
-      return assert(false, `${fnName} is not a function`)
-    }
+  it('Should handleError() work with EPERM', (resolve) => {
     const err: NodeJS.ErrnoException = new Error()
 
     err.code = EntryType.noAcessPermission
     err.path = Math.random().toString()
 
-    fn(err).subscribe(
+    handleError(err, 0).subscribe(
       (data) => {
         assert(data.type === EntryType.noAcessPermission)
         assert(data.path === err.path)
@@ -34,19 +23,12 @@ describe(fileShortPath(import.meta.url), () => {
     )
   })
 
-  it('Should handleError() works with ENOENT', (resolve) => {
-    const fnName = 'handleError'
-    const fn = mods.__get__(fnName)
-
-    if (typeof fn !== 'function') {
-      return assert(false, `${fnName} is not a function`)
-    }
+  it('Should handleError() work with ENOENT', (resolve) => {
     const err: NodeJS.ErrnoException = new Error()
-
     err.code = EntryType.notExist
     err.path = Math.random().toString()
 
-    fn(err).subscribe(
+    handleError(err, 0).subscribe(
       (data) => {
         assert(data.type === EntryType.notExist)
         assert(data.path === err.path)
@@ -55,19 +37,12 @@ describe(fileShortPath(import.meta.url), () => {
     )
   })
 
-  it('Should handleError() works with unknown', (resolve) => {
-    const fnName = 'handleError'
-    const fn = mods.__get__(fnName)
-
-    if (typeof fn !== 'function') {
-      return assert(false, `${fnName} is not a function`)
-    }
+  it('Should handleError() work with unknown', (resolve) => {
     const err: NodeJS.ErrnoException = new Error()
-
     err.code = EntryType.unknown
     err.path = Math.random().toString()
 
-    fn(err).subscribe(
+    handleError(err, 0).subscribe(
       (data) => {
         assert(data.type === EntryType.unknown)
         assert(data.path === err.path)
@@ -77,18 +52,11 @@ describe(fileShortPath(import.meta.url), () => {
   })
 
   it('Should handleError() works with unknown error.code', (resolve) => {
-    const fnName = 'handleError'
-    const fn = mods.__get__(fnName)
-
-    if (typeof fn !== 'function') {
-      return assert(false, `${fnName} is not a function`)
-    }
     const err: NodeJS.ErrnoException = new Error()
-
     err.code = 'whaterror'
     err.path = Math.random().toString()
 
-    fn(err).subscribe(
+    handleError(err, 0).subscribe(
       (data) => {
         assert(data.type === EntryType.unknown)
         assert(data.path === err.path)
