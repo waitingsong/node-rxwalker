55a4b671cbedf08b02ae36fb38c8327131334691
waiting <1661926154@qq.com>
2023-09-19 15:42:38 +0800

    build: breaking change pure ESM only

--- test/20.index.test.ts
+++ test/20.index.test.ts
@@ -1,6 +1,7 @@
 import assert from 'node:assert/strict'
+import { join } from 'node:path'
 
-import { join, fileShortPath, genCurrentDirname } from '@waiting/shared-core'
+import { fileShortPath } from '@waiting/shared-core'
 
 import {
   walk,
@@ -9,13 +10,13 @@ import {
   EntryType,
 } from '../src/index.js'
 
+import { testDir } from '#@/root.config.js'
 
-const testRootDir = genCurrentDirname(import.meta.url)
 
 describe(fileShortPath(import.meta.url), () => {
 
   it('Should walk() works', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     const assertDirCount = 7
     const assertFileCount = 8
     let dirCount = 0
@@ -23,8 +24,9 @@ describe(fileShortPath(import.meta.url), () => {
     let linkCount = 0
     let entryCount = 0
 
-    walk(path).subscribe(
-      (data) => {
+
+    walk(path).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -45,29 +47,31 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
   it('Should walk() works', (resolve) => {
-    const path = join(testRootDir, 'test_dirs/dir-level1-a')
+    const path = join(testDir, 'test_dirs/dir-level1-a')
     const assertDirCount = 2
     const assertFileCount = 2
     let dirCount = 0
     let fileCount = 0
     let entryCount = 0
 
-    walk(path).subscribe(
-      (data) => {
+    walk(path).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -84,29 +88,31 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
   it('Should walk() works with path not existing', (resolve) => {
-    const path = join(testRootDir, 'test_dirsFAKE')
+    const path = join(testDir, 'test_dirsFAKE')
     const assertDirCount = 0
     const assertFileCount = 0
     let dirCount = 0
     let fileCount = 0
     let entryCount = 0
 
-    walk(path).subscribe(
-      (data) => {
+    walk(path).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -123,22 +129,24 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
 
   it('Should walk() works with options.maxDepth == 0', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     const assertDirCount = 1
     const assertFileCount = 0
     let dirCount = 0
@@ -146,8 +154,8 @@ describe(fileShortPath(import.meta.url), () => {
     let entryCount = 0
     const maxDepth = 0
 
-    walk(path, { maxDepth }).subscribe(
-      (data) => {
+    walk(path, { maxDepth }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -164,21 +172,23 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
   it('Should walk() works with options.maxDepth == 1', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     const assertDirCount = 4
     const assertFileCount = 2
     let dirCount = 0
@@ -186,8 +196,8 @@ describe(fileShortPath(import.meta.url), () => {
     let entryCount = 0
     const maxDepth = 1
 
-    walk(path, { maxDepth }).subscribe(
-      (data) => {
+    walk(path, { maxDepth }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -203,23 +213,24 @@ describe(fileShortPath(import.meta.url), () => {
             entryCount += 1
             break
         }
+      },
 
-      },
-      (err) => {
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
   it('Should walk() works with DirFilterCb returining Array', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     const assertDirCount = 7
     const assertFileCount = 8
     let dirCount = 0
@@ -230,8 +241,8 @@ describe(fileShortPath(import.meta.url), () => {
       return files
     }
 
-    walk(path, { dirFilterCb }).subscribe(
-      (data) => {
+    walk(path, { dirFilterCb }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -248,21 +259,23 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
   it('Should walk() works with DirFilterCb returining Array filtered by name', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     // test_dirs, test_dirs/dir-level1-a, test_dirs/dir-level1-a/dir-level2-a
     const assertDirCount = 3
     // test_dirs/level1-a.txt, test_dirs/dir-level1-a/level2-a.txt, test_dirs/dir-level1-a/dir-level2-a/level3-a.txt
@@ -277,8 +290,8 @@ describe(fileShortPath(import.meta.url), () => {
       })
     }
 
-    walk(path, { dirFilterCb }).subscribe(
-      (data) => {
+    walk(path, { dirFilterCb }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -295,22 +308,24 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
 
   it('Should walk() works with DirFilterCb returining Array filtered by parentPath', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     // test_dirs, test_dirs/dir-level1-a, test_dirs/dir-level1-b, test_dirs/dir-level1-z,
     // dir-level1-a/dir-level2-a
     const assertDirCount = 5
@@ -332,8 +347,8 @@ describe(fileShortPath(import.meta.url), () => {
       }
     }
 
-    walk(path, { dirFilterCb }).subscribe(
-      (data) => {
+    walk(path, { dirFilterCb }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -350,22 +365,24 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
+
   })
 
 
   it('Should walk() works with DirFilterCb returining Array filtered by name and maxDepth', (resolve) => {
-    const path = join(testRootDir, 'test_dirs')
+    const path = join(testDir, 'test_dirs')
     const assertDirCount = 2 // test_dirs, test_dirs/dir-level1-a
     const assertFileCount = 1 // test_dirs/level1-a.txt
     let dirCount = 0
@@ -378,8 +395,8 @@ describe(fileShortPath(import.meta.url), () => {
       })
     }
 
-    walk(path, { dirFilterCb }).subscribe(
-      (data) => {
+    walk(path, { dirFilterCb }).subscribe({
+      next(data) {
         switch (data.type) {
           case EntryType.dir:
             dirCount += 1
@@ -396,17 +413,18 @@ describe(fileShortPath(import.meta.url), () => {
             break
         }
       },
-      (err) => {
+
+      error(err) {
         assert(false, (err as Error).message)
-        resolve()
       },
-      () => {
+
+      complete() {
         assertCount({
           path, dirCount, fileCount, entryCount, assertDirCount, assertFileCount,
         })
         resolve()
       },
-    )
+    })
   })
 
 
